a230f1454c0d57435bff86dedbd673205a1545b6
yelsharawy <yelsharawy20@stuy.edu>
2021-09-14 07:38:29 -0400

    Removed unnecessary exporting of templates

--- optick.nim
+++ optick.nim
@@ -30,41 +30,40 @@ type OptickCategory = cstring
 
 
 when defined release:
-    # {.link:"optick/release/OptickCore.lib"}
-    template optickFrame*(name : cstring) = discard
-    template optickEvent* = discard
-    template optickEvent*(name : cstring) = discard
-    template optickTag*(tag : cstring, value : int) = discard
-    template optickTag*(tag : cstring, value : float) = discard
-    template optickTag*(tag : cstring, value : auto) = discard
+    template optickFrame(name : cstring) = discard
+    template optickEvent = discard
+    template optickEvent(name : cstring) = discard
+    template optickTag(tag : cstring, value : int) = discard
+    template optickTag(tag : cstring, value : float) = discard
+    template optickTag(tag : cstring, value : auto) = discard
     
 else:
     {.emit:"#include \"optick/optick.h\"".}
     {.link:"optick/debug/OptickCore.lib"}
 
-    template optickFrame*(name : cstring) =
+    template optickFrame(name : cstring) =
         {.emit:["OPTICK_FRAME(\"",name,"\");"].}
 
-    template optickEvent* =
+    template optickEvent =
         {.emit:"OPTICK_EVENT();".}
     
-    template optickEvent*(name : cstring) =
+    template optickEvent(name : cstring) =
         {.emit:["OPTICK_EVENT(\"",name,"\");"].}
     
-    template optickGpuFlip*() =
+    template optickGpuFlip() =
         {.emit:["OPTICK_GPU_FLIP(0);"].}
     
-    template optickCategory*(category : OptickCategory) =
+    template optickCategory(category : OptickCategory) =
         {.emit:["OPTICK_CATEGORY(OPTICK_FUNC, Optick::Category::",`category`,");"].}
     
-    template optickCategory*(name : cstring, category : OptickCategory) =
+    template optickCategory(name : cstring, category : OptickCategory) =
         {.emit:["OPTICK_CATEGORY(\"",name,"\", Optick::Category::",`category`,");"].}
 
-    template optickTag*(tag : cstring, value : int) =
+    template optickTag(tag : cstring, value : int) =
         {.emit:["OPTICK_TAG(\"", `tag`, "\",(int)", `value`, ");"].}
 
-    template optickTag*(tag : cstring, value : float) =
+    template optickTag(tag : cstring, value : float) =
         {.emit:["OPTICK_TAG(\"", `tag`, "\",(float)", `value`, ");"].}
 
-    template optickTag*(tag : cstring, value : auto) =
+    template optickTag(tag : cstring, value : auto) =
         {.emit:["OPTICK_TAG(\"", `tag`, "\",", `value`, ");"].}
