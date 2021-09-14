3b717fd81136da84d700c24337253f5c2194b02f
yelsharawy <yelsharawy20@stuy.edu>
2021-09-14 09:52:05 -0400

    Added `used` pragma to all Optick templates

--- optick.nim
+++ optick.nim
@@ -1,5 +1,3 @@
-{.used.}
-
 # This really needs to be redone, probably through the use of macros
 # instead of templates, using `instantiationInfo()` for accurate func names.
 # And it's not great that this file is being included instead of imported...
@@ -30,40 +28,40 @@ type OptickCategory = cstring
 
 
 when defined release:
-    template optickFrame(name : cstring) = discard
-    template optickEvent = discard
-    template optickEvent(name : cstring) = discard
-    template optickTag(tag : cstring, value : int) = discard
-    template optickTag(tag : cstring, value : float) = discard
-    template optickTag(tag : cstring, value : auto) = discard
+    template optickFrame(name : cstring) {.used.} = discard
+    template optickEvent {.used.} = discard
+    template optickEvent(name : cstring) {.used.} = discard
+    template optickTag(tag : cstring, value : int) {.used.} = discard
+    template optickTag(tag : cstring, value : float) {.used.} = discard
+    template optickTag(tag : cstring, value : auto) {.used.} = discard
     
 else:
     {.emit:"#include \"optick/optick.h\"".}
     {.link:"optick/debug/OptickCore.lib"}
 
-    template optickFrame(name : cstring) =
+    template optickFrame(name : cstring) {.used.} =
         {.emit:["OPTICK_FRAME(\"",name,"\");"].}
 
-    template optickEvent =
+    template optickEvent {.used.} =
         {.emit:"OPTICK_EVENT();".}
     
-    template optickEvent(name : cstring) =
+    template optickEvent(name : cstring) {.used.} =
         {.emit:["OPTICK_EVENT(\"",name,"\");"].}
     
-    template optickGpuFlip() =
+    template optickGpuFlip() {.used.} =
         {.emit:["OPTICK_GPU_FLIP(0);"].}
     
-    template optickCategory(category : OptickCategory) =
+    template optickCategory(category : OptickCategory) {.used.} =
         {.emit:["OPTICK_CATEGORY(OPTICK_FUNC, Optick::Category::",`category`,");"].}
     
-    template optickCategory(name : cstring, category : OptickCategory) =
+    template optickCategory(name : cstring, category : OptickCategory) {.used.} =
         {.emit:["OPTICK_CATEGORY(\"",name,"\", Optick::Category::",`category`,");"].}
 
-    template optickTag(tag : cstring, value : int) =
+    template optickTag(tag : cstring, value : int) {.used.} =
         {.emit:["OPTICK_TAG(\"", `tag`, "\",(int)", `value`, ");"].}
 
-    template optickTag(tag : cstring, value : float) =
+    template optickTag(tag : cstring, value : float) {.used.} =
         {.emit:["OPTICK_TAG(\"", `tag`, "\",(float)", `value`, ");"].}
 
-    template optickTag(tag : cstring, value : auto) =
+    template optickTag(tag : cstring, value : auto) {.used.} =
         {.emit:["OPTICK_TAG(\"", `tag`, "\",", `value`, ");"].}
